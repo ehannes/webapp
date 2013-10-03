7081e27236e785fe638986cf457235c594d0782d
Hannes Elvemyr <hannes88@gmail.com>
2013-10-03 15:33:35 +0200

    Project started!

---
+++ webbapp_model/src/main/java/com/adde/webbapp_model/App.java
@@ -0,0 +1,13 @@
+package com.adde.webbapp_model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
