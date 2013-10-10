e87c6205f96924ce75b8b919b596560f022eff79
Hannes Elvemyr <hannes88@gmail.com>
2013-10-10 15:43:05 +0200

    Test!

---
+++ webapp_backend/src/java/com/adde/webapp_backend/Test.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adde.webapp_backend;
+
+/**
+ * This is a test!
+ * @author hannes
+ */
+public class Test {
+    
+}
