8c256651f958fbd50fdd3bcd210537136246146b
Hannes Elvemyr <hannes88@gmail.com>
2013-10-14 14:27:28 +0200

    AbstractEntity added.

---
+++ webbapp_model/src/main/java/com/adde/webbapp_model/backend/AbstractEntity.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adde.webbapp_model.backend;
+
+/**
+ *
+ * @author hannes
+ */
+public class AbstractEntity {
+    
+}
