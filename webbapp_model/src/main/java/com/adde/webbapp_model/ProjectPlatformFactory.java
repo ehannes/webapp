1a7a31b8670031b166e2cd598e66d3806721f155
Eric Ahlberg <eahlberg@gmail.com>
2013-10-15 15:54:52 +0200

    Removed static test data in ProjectPlatformFactory

--- webbapp_model/src/main/java/com/adde/webbapp_model/ProjectPlatformFactory.java
+++ webbapp_model/src/main/java/com/adde/webbapp_model/ProjectPlatformFactory.java
@@ -6,42 +6,10 @@ package com.adde.webbapp_model;
  */
 public class ProjectPlatformFactory {
     
-    // Total number of owners
-    private final static int OWNERS = 5;
-    
-    // Collaborators per project
-    private final static int COLLABORATORS_PER_PROJECT = 3;
-    
     private ProjectPlatformFactory() {
     }
 
-    public static ProjectPlatform getProjectPlatform(boolean initTestData, String puName) {
-        ProjectPlatform p = new ProjectPlatform(puName);
-        if (initTestData) {
-            initTestData(p);
-        }
-        return p;
-    }
-    
-    private static void initTestData(ProjectPlatform p) {
-        try {
-            for(int i = 0; i < OWNERS; i++) {
-                Person owner = new Person("Owner " + i, "Email" + i);
-                p.addUser(owner);
-                Project project = new Project("Test project " + i, owner);
-                for (int k = 0; k < COLLABORATORS_PER_PROJECT; k++) {
-                    Person collaborator = new Person("Collaborator " + k, "Email" + k);
-                    p.addUser(collaborator);
-                    project.addCollaborator(collaborator);
-                }
-                p.addProject(project);
-            }
-        } catch (NullPointerException e) {
-            System.out.println("Fail in initTestData");
-            System.out.println(e.toString());
-        } catch (Exception e) {
-            System.out.println("Fail in initTestData");
-            System.out.println(e.toString());
-        }
+    public static ProjectPlatform getProjectPlatform(String puName) {
+        return new ProjectPlatform(puName);
     }
 }
