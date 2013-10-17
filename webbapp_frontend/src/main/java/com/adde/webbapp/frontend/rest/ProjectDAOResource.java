2bf4bac14c623d38c1b693ca9c755212077eb698
Joakim Danielsson <joakimdanielsson0@gmail.com>
2013-10-17 17:52:29 +0200

    ProjectDAOResource, PrimitiveJSONWrapper and projectProxy implemented

--- webbapp_frontend/src/main/java/com/adde/webbapp/frontend/rest/ProjectDAOResource.java
+++ webbapp_frontend/src/main/java/com/adde/webbapp/frontend/rest/ProjectDAOResource.java
@@ -4,11 +4,121 @@
  */
 package com.adde.webbapp.frontend.rest;
 
+import com.adde.webbapp.model.dao.DAOFactory;
+import com.adde.webbapp.model.dao.ProjectDAO;
+import com.adde.webbapp.model.entity.Person;
+import com.adde.webbapp.model.entity.Project;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
 /**
  *
  * @author Joakim
  */
+ @Path("/inside/projects")
 public class ProjectDAOResource {
+    private final ProjectDAO projectDAO = DAOFactory.getDAOFactory().getProjectDAO();
     
+    @Context
+    private UriInfo uriInfo;
+   
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response get() {
+        List<Project> projects = projectDAO.getAll();
+        return Response.ok(toProductProxy(projects)).build();
+    }
+    
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Path("{id}")
+    public Response find(@PathParam("id") long id) {
+        Project project = projectDAO.find(id);
+                GenericEntity<ProjectProxy> ge = new GenericEntity<ProjectProxy>(new ProjectProxy(project)) {
+        };
+           
+        if (project != null) {
+            return Response.ok(ge).build();
+        } else {
+            return Response.noContent().build();
+        }
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response add(@FormParam("name") String name,
+            @FormParam("admin") Person admin) {
+        Project p = new Project(name, admin);
+        try {
+            projectDAO.add(p);
+            
+            URI uri = uriInfo.getAbsolutePathBuilder().path(String.valueOf(p.getId())).build(p);
+            return Response.created(uri).build();
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    
+ 
+    @DELETE
+    @Path("{id}")
+    public Response remove(@PathParam("id") long id) {
+        try {
+            projectDAO.remove(id);
+            return Response.ok().build();
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response update(@FormParam("name") String name,
+            @FormParam("admin") Person admin) {
+        try {
+            Project updatedProject = new Project(name, admin);
+            projectDAO.update(updatedProject);
+            return Response.ok(new ProjectProxy(updatedProject)).build();
+        } catch (IllegalArgumentException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+ 
+    @GET
+    @Path("count")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response getCount() {
+        return Response.ok(new PrimitiveJSONWrapper(projectDAO.getCount())).build();
+    }
+
+    
+    private GenericEntity<List<ProjectProxy>> toProductProxy(List<Project> projects) {
+        List<ProjectProxy> projectProxies = new ArrayList<>();
+        for (Project p : projects) {
+            projectProxies.add(new ProjectProxy(p));
+        }
+        GenericEntity<List<ProjectProxy>> ge = new GenericEntity<List<ProjectProxy>>(projectProxies) {
+        };
+        return ge;
+    }
     
 }
---
+++ webbapp_frontend/src/main/java/com/adde/webbapp/frontend/rest/ProjectProxy.java
@@ -0,0 +1,71 @@
+package com.adde.webbapp.frontend.rest;
+
+import com.adde.webbapp.model.entity.Article;
+import com.adde.webbapp.model.entity.Person;
+import com.adde.webbapp.model.entity.Project;
+import com.adde.webbapp.model.entity.TodoPost;
+import com.adde.webbapp.model.entity.WallPost;
+import java.util.Calendar;
+import java.util.List;
+import javax.xml.bind.annotation.*;
+
+/**
+ * Need this because translation from XML to JSON
+ *
+ * @author hajo
+ */
+@XmlRootElement(name = "project")
+//@XmlAccessorType(XmlAccessType.PROPERTY)
+@XmlType(name = "ProductType", propOrder = {})
+public class ProjectProxy {
+
+    // The wrapped project
+    private Project project;
+
+    protected ProjectProxy() { // Must have
+    }
+
+    public ProjectProxy(Project project) {
+        this.project = project;
+    }
+
+    @XmlElement(required = true)
+    public String getName() {
+        return project.getName();
+    }
+
+    @XmlElement(required = true)
+    public Long getId() {
+        return project.getId();
+    }
+
+    @XmlElement(required = true)
+    public Person getPrice() {
+        return project.getAdmin();
+    }
+
+    @XmlElement(required = true)
+    public List<Person> getCollaborators() {
+        return project.getCollaborators();
+    }
+
+    @XmlElement(required = true)
+    public List<Article> getArticles() {
+        return project.getArticles();
+    }
+
+    @XmlElement(required = true)
+    public List<TodoPost> getTodoPosts() {
+        return project.getTodoPosts();
+    }
+
+    @XmlElement(required = true)
+    public List<WallPost> getWallPosts() {
+        return project.getWallPosts();
+    }
+
+    @XmlElement(required = true)
+    public Calendar getDateCreated() {
+        return project.getDateCreated();
+    }
+}
