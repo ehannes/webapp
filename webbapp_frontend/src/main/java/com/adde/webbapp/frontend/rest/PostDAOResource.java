719d34a9d983dbedc6b2c64b1deb35fd2ca1e10d
gustav <gustav@gustav-UX31E>
2013-10-17 17:46:54 +0200

    Syncing PostDAOResource to collaborators... pretty much an empty class.

---
+++ webbapp_frontend/src/main/java/com/adde/webbapp/frontend/rest/PostDAOResource.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.adde.webbapp.frontend.rest;
+
+import javax.ws.rs.Path;
+
+@Path("/inside/post")
+public class PostDAOResource {
+    
+}
