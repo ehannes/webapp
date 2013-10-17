686bf6658d0c61c2427cd3b0e4247d0e50e84372
Hannes Elvemyr <hannes88@gmail.com>
2013-10-17 17:11:20 +0200

    Login functionality added, though not possible to compile!!!

---
+++ webbapp_frontend/src/main/java/com/adde/webbapp/frontend/servlet/auth/AuthServlet.java
@@ -0,0 +1,98 @@
+package com.adde.webbapp.frontend.servlet.auth;
+
+import com.adde.webbapp.frontend.servlet.DAOFactoryWrapper;
+import com.adde.webbapp.model.dao.PersonDAO;
+import com.adde.webbapp.model.entity.Person;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author ehannes
+ */
+@WebServlet(urlPatterns = {"/auth"})
+public class AuthServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        DAOFactoryWrapper daoFactoryWrapper = (DAOFactoryWrapper) request.getServletContext().getAttribute("DAOFACTORY");
+        PersonDAO personDAO = daoFactoryWrapper.getPersonDAO();
+        String action = request.getParameter("action");
+
+        if (action != null) {
+            if (action.equals("login")) {
+                String username = request.getParameter("username");
+                String password = request.getParameter("password");
+                if (password != null && username != null) {
+                    Person person = personDAO.getByUserName(username);
+                    if (person != null
+                            && person.getPassword().equals(password)) {
+                        HttpSession session = request.getSession(true);
+                        session.setAttribute("user", person);
+                        response.sendRedirect("fc?view=welcome");
+                    }
+                }
+                request.getRequestDispatcher("WEB-INF/jsp/login");
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
