408eb31758a8d94e95ee596d191c291426b3229d
Joakim Danielsson <joakimdanielsson0@gmail.com>
2013-10-17 16:36:53 +0200

    get function for all DAOs

--- webbapp_frontend/src/main/java/com/adde/webbapp/frontend/servlet/DAOFactoryWrapper.java
+++ webbapp_frontend/src/main/java/com/adde/webbapp/frontend/servlet/DAOFactoryWrapper.java
@@ -4,24 +4,54 @@
  */
 package com.adde.webbapp.frontend.servlet;
 
+import com.adde.webbapp.model.dao.ArticleDAO;
 import com.adde.webbapp.model.dao.DAOFactory;
 import com.adde.webbapp.model.dao.PersonDAO;
+import com.adde.webbapp.model.dao.PostDAO;
+import com.adde.webbapp.model.dao.ProjectDAO;
+import com.adde.webbapp.model.dao.SimpleEditorEntryDAO;
+import com.adde.webbapp.model.dao.TodoPostDAO;
+import com.adde.webbapp.model.dao.WallPostDAO;
 
 /**
- * This is a wrapper to make the shop a singleton
- * Could have used CDI (?) but too much for now, introduced later
+ * This is a wrapper to make the shop a singleton Could have used CDI (?) but
+ * too much for now, introduced later
+ *
  * @author Eric Ahlberg (eahlberg@gmail.com)
  */
 public enum DAOFactoryWrapper {
+
     INSTANCE;
-    
     private final DAOFactory d;
-    
-    private DAOFactoryWrapper(){
-       d = DAOFactory.getDAOFactory();
+
+    private DAOFactoryWrapper() {
+        d = DAOFactory.getDAOFactory();
     }
-    
+
     public PersonDAO getPersonDAO() {
         return d.getPersonDAO();
+    }
+
+    public ProjectDAO getProjectDAO() {
+        return d.getProjectDAO();
+    }
+
+    public PostDAO getPostDAO() {
+        return d.getPostDAO();
+    }
+
+    public TodoPostDAO getTodoPostDAO() {
+        return d.getTodoPostDAO();
+    }
+
+    public ArticleDAO getArticleDAO() {
+        return d.getArticleDAO();
+    }
+
+    public WallPostDAO getWallPostDAO() {
+        return d.getWallPostDAO();
+    }
+    public SimpleEditorEntryDAO getSimpleEditorEntryDAO() {
+        return d.getSimpleEditorEntryDAO();
     }
 }
