5c22a9e8112f403e2d6e8289d812e866b4e5a514
Eric Ahlberg <eahlberg@gmail.com>
2013-10-18 15:26:22 +0200

    Commented out classes that made the deployment fail

--- webbapp_frontend/src/main/java/com/adde/webbapp/frontend/rest/ProjectCatalogueResource.java
+++ webbapp_frontend/src/main/java/com/adde/webbapp/frontend/rest/ProjectCatalogueResource.java
@@ -1,122 +1,122 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.adde.webbapp.frontend.rest;
-
-import com.adde.webbapp.model.dao.DAOFactory;
-import com.adde.webbapp.model.dao.ProjectCatalogue;
-import com.adde.webbapp.model.entity.Person;
-import com.adde.webbapp.model.entity.Project;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
-
-/**
- *
- * @author Joakim
- */
-@Path("projects")
-public class ProjectCatalogueResource {
-
-    private final ProjectCatalogue projectCatalogue = DAOFactory.getDAOFactory().getProjectDAO();
-    @Context
-    private UriInfo uriInfo;
-
-    @GET
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response get() {
-        List<Project> projects = projectCatalogue.getAll();
-        return Response.ok(toProjectProxy(projects)).build();
-    }
-
-    @GET
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    @Path("{id}")
-    public Response find(@PathParam("id") long id) {
-        Project project = projectCatalogue.find(id);
-        GenericEntity<ProjectProxy> ge = new GenericEntity<ProjectProxy>(new ProjectProxy(project)) {
-        };
-
-        if (project != null) {
-            return Response.ok(ge).build();
-        } else {
-            return Response.noContent().build();
-        }
-    }
-
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response add(@FormParam("name") String name,
-            @FormParam("admin") Person admin) {
-        Project p = new Project(name, admin);
-        try {
-            projectCatalogue.add(p);
-
-            URI uri = uriInfo.getAbsolutePathBuilder().path(String.valueOf(p.getId())).build(p);
-            return Response.created(uri).build();
-        } catch (IllegalArgumentException e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-
-    @DELETE
-    @Path("{id}")
-    public Response remove(@PathParam("id") long id) {
-        try {
-            projectCatalogue.remove(id);
-            return Response.ok().build();
-        } catch (IllegalArgumentException e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-
-    @PUT
-    @Path("{id}")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response update(@PathParam("id") Long id, @FormParam("name") String name,
-            @FormParam("admin") Person admin) {
-        Project oldProject = projectCatalogue.find(id);
-        if (oldProject != null) {
-            oldProject.setAdmin(admin);
-            oldProject.setName(name);
-            projectCatalogue.update(oldProject);
-            return Response.ok(new ProjectProxy(oldProject)).build();
-        }
-        return Response.notModified("Project not found").build();
-    }
-
-    @GET
-    @Path("count")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response getCount() {
-        return Response.ok(new PrimitiveJSONWrapper(projectCatalogue.getCount())).build();
-    }
-
-    private GenericEntity<List<ProjectProxy>> toProjectProxy(List<Project> projects) {
-        List<ProjectProxy> projectProxies = new ArrayList<>();
-        for (Project p : projects) {
-            projectProxies.add(new ProjectProxy(p));
-        }
-        GenericEntity<List<ProjectProxy>> ge = new GenericEntity<List<ProjectProxy>>(projectProxies) {
-        };
-        return ge;
-    }
-}
+///*
+// * To change this template, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package com.adde.webbapp.frontend.rest;
+//
+//import com.adde.webbapp.model.dao.DAOFactory;
+//import com.adde.webbapp.model.dao.ProjectCatalogue;
+//import com.adde.webbapp.model.entity.Person;
+//import com.adde.webbapp.model.entity.Project;
+//import java.net.URI;
+//import java.util.ArrayList;
+//import java.util.List;
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.DELETE;
+//import javax.ws.rs.DefaultValue;
+//import javax.ws.rs.FormParam;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.PUT;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.PathParam;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.QueryParam;
+//import javax.ws.rs.core.Context;
+//import javax.ws.rs.core.GenericEntity;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Response;
+//import javax.ws.rs.core.UriInfo;
+//
+///**
+// *
+// * @author Joakim
+// */
+//@Path("projects")
+//public class ProjectCatalogueResource {
+//
+//    private final ProjectCatalogue projectCatalogue = DAOFactory.getDAOFactory().getProjectDAO();
+//    @Context
+//    private UriInfo uriInfo;
+//
+//    @GET
+//    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+//    public Response get() {
+//        List<Project> projects = projectCatalogue.getAll();
+//        return Response.ok(toProjectProxy(projects)).build();
+//    }
+//
+//    @GET
+//    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+//    @Path("{id}")
+//    public Response find(@PathParam("id") long id) {
+//        Project project = projectCatalogue.find(id);
+//        GenericEntity<ProjectProxy> ge = new GenericEntity<ProjectProxy>(new ProjectProxy(project)) {
+//        };
+//
+//        if (project != null) {
+//            return Response.ok(ge).build();
+//        } else {
+//            return Response.noContent().build();
+//        }
+//    }
+//
+//    @POST
+//    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//    public Response add(@FormParam("name") String name,
+//            @FormParam("admin") Person admin) {
+//        Project p = new Project(name, admin);
+//        try {
+//            projectCatalogue.add(p);
+//
+//            URI uri = uriInfo.getAbsolutePathBuilder().path(String.valueOf(p.getId())).build(p);
+//            return Response.created(uri).build();
+//        } catch (IllegalArgumentException e) {
+//            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+//        }
+//    }
+//
+//    @DELETE
+//    @Path("{id}")
+//    public Response remove(@PathParam("id") long id) {
+//        try {
+//            projectCatalogue.remove(id);
+//            return Response.ok().build();
+//        } catch (IllegalArgumentException e) {
+//            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+//        }
+//    }
+//
+//    @PUT
+//    @Path("{id}")
+//    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+//    public Response update(@PathParam("id") Long id, @FormParam("name") String name,
+//            @FormParam("admin") Person admin) {
+//        Project oldProject = projectCatalogue.find(id);
+//        if (oldProject != null) {
+//            oldProject.setAdmin(admin);
+//            oldProject.setName(name);
+//            projectCatalogue.update(oldProject);
+//            return Response.ok(new ProjectProxy(oldProject)).build();
+//        }
+//        return Response.notModified("Project not found").build();
+//    }
+//
+//    @GET
+//    @Path("count")
+//    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+//    public Response getCount() {
+//        return Response.ok(new PrimitiveJSONWrapper(projectCatalogue.getCount())).build();
+//    }
+//
+//    private GenericEntity<List<ProjectProxy>> toProjectProxy(List<Project> projects) {
+//        List<ProjectProxy> projectProxies = new ArrayList<>();
+//        for (Project p : projects) {
+//            projectProxies.add(new ProjectProxy(p));
+//        }
+//        GenericEntity<List<ProjectProxy>> ge = new GenericEntity<List<ProjectProxy>>(projectProxies) {
+//        };
+//        return ge;
+//    }
+//}
--- webbapp_frontend/src/main/java/com/adde/webbapp/frontend/rest/WallPostCatalogueResource.java
+++ webbapp_frontend/src/main/java/com/adde/webbapp/frontend/rest/WallPostCatalogueResource.java
@@ -1,121 +1,121 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.adde.webbapp.frontend.rest;
-
-import com.adde.webbapp.model.dao.DAOFactory;
-import com.adde.webbapp.model.dao.WallPostCatalogue;
-import com.adde.webbapp.model.entity.Person;
-import com.adde.webbapp.model.entity.WallPost;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
-
-/**
- *
- * @author Joakim
- */
-@Path("wallposts")
-public class WallPostCatalogueResource {
-    
-    private final WallPostCatalogue wallPostCatalogue = DAOFactory.getDAOFactory().getWallPostDAO();
-    @Context
-    private UriInfo uriInfo;
-
-    @GET
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response get() {
-        List<WallPost> wallPosts = wallPostCatalogue.getAll();
-        return Response.ok(toWallPostProxy(wallPosts)).build();
-    }
-
-    @GET
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    @Path("{id}")
-    public Response find(@PathParam("id") long id) {
-        WallPost wallPost = wallPostCatalogue.find(id);
-        GenericEntity<WallPostProxy> ge = new GenericEntity<WallPostProxy>(new WallPostProxy(wallPost)) {
-        };
-
-        if (wallPost != null) {
-            return Response.ok(ge).build();
-        } else {
-            return Response.noContent().build();
-        }
-    }
-
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    public Response add(@FormParam("author") Person author,
-            @FormParam("msg") String msg) {
-        WallPost w = new WallPost(author, msg);
-        try {
-            wallPostCatalogue.add(w);
-
-            URI uri = uriInfo.getAbsolutePathBuilder().path(String.valueOf(w.getId())).build(w);
-            return Response.created(uri).build();
-        } catch (IllegalArgumentException e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-
-    @DELETE
-    @Path("{id}")
-    public Response remove(@PathParam("id") long id) {
-        try {
-            wallPostCatalogue.remove(id);
-            return Response.ok().build();
-        } catch (IllegalArgumentException e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
-        }
-    }
-
-    @PUT
-    @Path("{id}")
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response update(@PathParam("id") Long id, @FormParam("author") Person author,
-            @FormParam("msg") String msg) {
-        WallPost oldWallPost = wallPostCatalogue.find(id);
-        if (oldWallPost != null) {
-            oldWallPost.setAuthor(author);
-            oldWallPost.setMsg(msg);
-            wallPostCatalogue.update(oldWallPost);
-            return Response.ok(new WallPostProxy(oldWallPost)).build();
-        }
-        return Response.notModified("WallPost not found").build();
-    }
-
-    @GET
-    @Path("count")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Response getCount() {
-        return Response.ok(new PrimitiveJSONWrapper(wallPostCatalogue.getCount())).build();
-    }
-
-    private GenericEntity<List<WallPostProxy>> toWallPostProxy(List<WallPost> wallPosts) {
-        List<WallPostProxy> wallPostProxies = new ArrayList<>();
-        for (WallPost w : wallPosts) {
-            wallPostProxies.add(new WallPostProxy(w));
-        }
-        GenericEntity<List<WallPostProxy>> ge = new GenericEntity<List<WallPostProxy>>(wallPostProxies) {
-        };
-        return ge;
-    }
-    
-}
+///*
+// * To change this template, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package com.adde.webbapp.frontend.rest;
+//
+//import com.adde.webbapp.model.dao.DAOFactory;
+//import com.adde.webbapp.model.dao.WallPostCatalogue;
+//import com.adde.webbapp.model.entity.Person;
+//import com.adde.webbapp.model.entity.WallPost;
+//import java.net.URI;
+//import java.util.ArrayList;
+//import java.util.List;
+//import javax.ws.rs.Consumes;
+//import javax.ws.rs.DELETE;
+//import javax.ws.rs.FormParam;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.POST;
+//import javax.ws.rs.PUT;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.PathParam;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.core.Context;
+//import javax.ws.rs.core.GenericEntity;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.Response;
+//import javax.ws.rs.core.UriInfo;
+//
+///**
+// *
+// * @author Joakim
+// */
+//@Path("wallposts")
+//public class WallPostCatalogueResource {
+//    
+//    private final WallPostCatalogue wallPostCatalogue = DAOFactory.getDAOFactory().getWallPostDAO();
+//    @Context
+//    private UriInfo uriInfo;
+//
+//    @GET
+//    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+//    public Response get() {
+//        List<WallPost> wallPosts = wallPostCatalogue.getAll();
+//        return Response.ok(toWallPostProxy(wallPosts)).build();
+//    }
+//
+//    @GET
+//    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+//    @Path("{id}")
+//    public Response find(@PathParam("id") long id) {
+//        WallPost wallPost = wallPostCatalogue.find(id);
+//        GenericEntity<WallPostProxy> ge = new GenericEntity<WallPostProxy>(new WallPostProxy(wallPost)) {
+//        };
+//
+//        if (wallPost != null) {
+//            return Response.ok(ge).build();
+//        } else {
+//            return Response.noContent().build();
+//        }
+//    }
+//
+//    @POST
+//    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//    public Response add(@FormParam("author") Person author,
+//            @FormParam("msg") String msg) {
+//        WallPost w = new WallPost(author, msg);
+//        try {
+//            wallPostCatalogue.add(w);
+//
+//            URI uri = uriInfo.getAbsolutePathBuilder().path(String.valueOf(w.getId())).build(w);
+//            return Response.created(uri).build();
+//        } catch (IllegalArgumentException e) {
+//            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+//        }
+//    }
+//
+//    @DELETE
+//    @Path("{id}")
+//    public Response remove(@PathParam("id") long id) {
+//        try {
+//            wallPostCatalogue.remove(id);
+//            return Response.ok().build();
+//        } catch (IllegalArgumentException e) {
+//            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+//        }
+//    }
+//
+//    @PUT
+//    @Path("{id}")
+//    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+//    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+//    public Response update(@PathParam("id") Long id, @FormParam("author") Person author,
+//            @FormParam("msg") String msg) {
+//        WallPost oldWallPost = wallPostCatalogue.find(id);
+//        if (oldWallPost != null) {
+//            oldWallPost.setAuthor(author);
+//            oldWallPost.setMsg(msg);
+//            wallPostCatalogue.update(oldWallPost);
+//            return Response.ok(new WallPostProxy(oldWallPost)).build();
+//        }
+//        return Response.notModified("WallPost not found").build();
+//    }
+//
+//    @GET
+//    @Path("count")
+//    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+//    public Response getCount() {
+//        return Response.ok(new PrimitiveJSONWrapper(wallPostCatalogue.getCount())).build();
+//    }
+//
+//    private GenericEntity<List<WallPostProxy>> toWallPostProxy(List<WallPost> wallPosts) {
+//        List<WallPostProxy> wallPostProxies = new ArrayList<>();
+//        for (WallPost w : wallPosts) {
+//            wallPostProxies.add(new WallPostProxy(w));
+//        }
+//        GenericEntity<List<WallPostProxy>> ge = new GenericEntity<List<WallPostProxy>>(wallPostProxies) {
+//        };
+//        return ge;
+//    }
+//    
+//}
