325e4cf33b3804947a81623138e0384fd7631f26
Joakim Danielsson <joakimdanielsson0@gmail.com>
2013-10-20 19:14:22 +0200

    add methods implemented in articles.js and projects.js

--- webbapp_frontend/src/main/webapp/js/gui/articles.js
+++ webbapp_frontend/src/main/webapp/js/gui/articles.js
@@ -2,16 +2,16 @@
 $(function() {
 
     var ac = projectPlatform.getArticleCatalogue();
-    
+
     $("#showArticles")
             .button()
             .click(function() {
         var deferred = ac.get();
-        
+
         deferred.done(function(articles) {
             $.each(articles.Article, function(index, a) {
                 console.log(a);
-                $("#data").append("<p>" +  " id: " + a.id + " <br/>title: " +
+                $("#data").append("<p>" + " id: " + a.id + " <br/>title: " +
                         a.title + " content: " + a.content + "</p>");
             });
             console.log(articles);
@@ -21,6 +21,52 @@ $(function() {
         });
     });
 
+    $("#addArticle")
+            .button()
+            .click(function() {
+        createAddDialog();
+    });
+    
+    function createAddDialog() {
+        clearValidationErrors();
+        clearFormDialogData();
+        var message = "Title and content";
+        $("#dialog-form").dialog({
+            autoOpen: true,
+            modal: true,
+            stack: true,
+            buttons: {
+                "Save": function() {
+                    var article = getFormDialogData();
+                        ac.add(article.title, article.content);
+                        $(this).dialog("close");
+                },
+                "Cancel": function() {
+                    $(this).dialog("close");
+                }
+            }
+        });
+        $('#dialog-message').dialog('option', 'title', 'Delete article');
+        $("#dialog-message #msg").text(message);
+        return $('#dialog-message');
+    }
+
+    function clearValidationErrors() {
+        $("#dialog-form input").each(function() {
+            $(this).removeClass("ui-state-error");
+        });
+    }
+
+    function getFormDialogData() {
+        var article = {};
+        article.title = $("#dialog-form #title").val();
+        article.content = $("#dialog-form #content").val();
+        return article;
+    }
+    function clearFormDialogData() {
+        $("#dialog-form #title").val("");
+        $("#dialog-form #title").val("");
+    }
 });
 
 //@ sourceURL=articles.js
--- webbapp_frontend/src/main/webapp/js/gui/projects.js
+++ webbapp_frontend/src/main/webapp/js/gui/projects.js
@@ -21,61 +21,52 @@ $(function() {
             console.log("fail");
         });
     });
-//    $("#addProject")
-//            .button()
-//            .click(function() {
-//        createAddDialog();
-//    });
-//
-//    function createAddDialog() {
-//        clearValidationErrors();
-//        clearFormDialogData();
-//        var message = "Type in your projects name";
-//        $("#dialog-form").dialog({
-//            autoOpen: true,
-//            modal: true,
-//            stack: true,
-//            buttons: {
-//                "Save": function() {
-//                    var project = getFormDialogData();
-//                    if (validate(project)) {
-//                        pc.add(project.name);
-//                        $(this).dialog("close");
-//                    }
-//                },
-//                "Cancel": function() {
-//                    $(this).dialog("close");
-//                }
-//            }
-//        });
-//        $('#dialog-message').dialog('option', 'title', 'Delete product');
-//        $("#dialog-message #msg").text(message);
-//        return $('#dialog-message');
-//    }
-//    function validate(project) {
-//        var regex = /^[1-9]\d*(((,\d{3}){1})?(\.\d{0,2})?)$/; // A number
-//        if (!regex.test(project.name)) {
-//            $("#dialog-form #name").addClass("ui-state-error");
-//            return false;
-//        }
-//        return true;
-//    }
-//
-//    function clearValidationErrors() {
-//        $("#dialog-form input").each(function() {
-//            $(this).removeClass("ui-state-error");
-//        });
-//    }
-//
-//    function getFormDialogData() {
-//        var project = {};
-//        project.name = $("#dialog-form #name").val();
-//        return project;
-//    }
-//
-//    function clearFormDialogData() {
-//        $("#dialog-form #name").val("");
-//    }
+    $("#addProject")
+            .button()
+            .click(function() {
+        createAddDialog();
+    });
+
+    function createAddDialog() {
+        clearValidationErrors();
+        clearFormDialogData();
+        var message = "Type in your projects name";
+        $("#dialog-form").dialog({
+            autoOpen: true,
+            modal: true,
+            stack: true,
+            buttons: {
+                "Save": function() {
+                    var project = getFormDialogData();
+
+                    pc.add(project.name);
+                    $(this).dialog("close");
+                },
+                "Cancel": function() {
+                    $(this).dialog("close");
+                }
+            }
+        });
+        $('#dialog-message').dialog('option', 'title', 'Delete project');
+        $("#dialog-message #msg").text(message);
+        return $('#dialog-message');
+    }
+
+    function clearValidationErrors() {
+        $("#dialog-form input").each(function() {
+            $(this).removeClass("ui-state-error");
+        });
+    }
+
+    function getFormDialogData() {
+        var project = {};
+        project.name = $("#dialog-form #name").val();
+        return project;
+    }
+
+    function clearFormDialogData() {
+        $("#dialog-form #name").val("");
+    }
 
 });
 
