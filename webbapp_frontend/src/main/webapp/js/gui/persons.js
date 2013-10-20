427779c7ff935e3b0b8a9655033839d1d0277d02
Eric Ahlberg <eahlberg@gmail.com>
2013-10-20 17:07:38 +0200

    Template for the logged in part

--- webbapp_frontend/src/main/webapp/js/gui/persons.js
+++ webbapp_frontend/src/main/webapp/js/gui/persons.js
@@ -6,15 +6,14 @@ $(function() {
     $("#showPersons")
             .button()
             .click(function() {
+        $("tbody").empty();
         var deferred = pc.get();
         
         deferred.done(function(persons) {
             $.each(persons.Person, function(index, p) {
                 console.log(p);
-                $("#data").append("<p>" + "date created: " + p.dateCreated + 
-                        " id: " + p.id + " <br/>email: " + p.email + 
-                        " username: " + p.username + " password: " + p.password +
-                        "</p>");
+                $("tbody").append("<tr> <td>" + p.userName + "</td> <td>" + 
+                    p.email + "</td></tr>");
             });
             console.log(persons);
         });
