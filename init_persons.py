087f79196b2cbe3603454ac04897a62beb2e6865
Hannes <hannes88@gmail.com>
2013-10-20 16:54:53 +0200

    Update init_persons.py

--- init_persons.py
+++ init_persons.py
@@ -1,12 +1,12 @@
 import os
 
 # Testing the Person resource
-base_url = 'http://localhost:8080/webbapp_frontend/rs/'
+base_url = 'http://localhost:8080/webbapp_frontend/content/rs/'
 resource = 'persons'
 LIMIT = 10
 
 for x in xrange(0, LIMIT):
     user_string = ("username=user" + str(x) + "&email=user" + str(x) + "@user"
-                   + str(x) + ".com" + "&password=pAssWORd!" + str(x))
+                   + str(x) + ".com" + "&password=pAssWORd" + str(x))
     adduser = 'curl --data ' + '"' + user_string + '"' + ' '
     os.system(adduser + base_url + resource)
