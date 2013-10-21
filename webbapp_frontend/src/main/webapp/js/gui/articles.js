1480b924b6aa7eb8fef8e44e35601fea8de3fe33
Eric Ahlberg <eahlberg@gmail.com>
2013-10-21 16:29:47 +0200

    Fixed a couple of errors in the Javascript

--- webbapp_frontend/src/main/webapp/js/gui/articles.js
+++ webbapp_frontend/src/main/webapp/js/gui/articles.js
@@ -1,72 +1,28 @@
-
 $(function() {
 
     var ac = projectPlatform.getArticleCatalogue();
-
+    
     $("#showArticles")
             .button()
             .click(function() {
+        $("#articles").empty();
         var deferred = ac.get();
-
+        
         deferred.done(function(articles) {
-            $.each(articles.Article, function(index, a) {
-                console.log(a);
-                $("#data").append("<p>" + " id: " + a.id + " <br/>title: " +
-                        a.title + " content: " + a.content + "</p>");
-            });
             console.log(articles);
+            $.each(articles.article, function(index, a) {
+                $("#articles").append("<div><h3>" + a.title+ "</h3></div>");
+                $("#articles").append("<div><p>" + a.content + "</p>" + 
+                        "<footer class='text-right'><i><small> Last edited: " + 
+                        a.articleEditions.editTime + "</small></i></footer></div>");
+            });
+            console.log(projects);
         });
         deferred.fail(function() {
             console.log("fail");
         });
     });
 
-    $("#addArticle")
-            .button()
-            .click(function() {
-        createAddDialog();
-    });
-    
-    function createAddDialog() {
-        clearValidationErrors();
-        clearFormDialogData();
-        var message = "Title and content";
-        $("#dialog-form").dialog({
-            autoOpen: true,
-            modal: true,
-            stack: true,
-            buttons: {
-                "Save": function() {
-                    var article = getFormDialogData();
-                        ac.add(article.title, article.content);
-                        $(this).dialog("close");
-                },
-                "Cancel": function() {
-                    $(this).dialog("close");
-                }
-            }
-        });
-        $('#dialog-message').dialog('option', 'title', 'Delete article');
-        $("#dialog-message #msg").text(message);
-        return $('#dialog-message');
-    }
-
-    function clearValidationErrors() {
-        $("#dialog-form input").each(function() {
-            $(this).removeClass("ui-state-error");
-        });
-    }
-
-    function getFormDialogData() {
-        var article = {};
-        article.title = $("#dialog-form #title").val();
-        article.content = $("#dialog-form #content").val();
-        return article;
-    }
-    function clearFormDialogData() {
-        $("#dialog-form #title").val("");
-        $("#dialog-form #content").val("");
-    }
 });
 
 //@ sourceURL=articles.js
--- webbapp_frontend/src/main/webapp/js/gui/projects.js
+++ webbapp_frontend/src/main/webapp/js/gui/projects.js
@@ -1,19 +1,18 @@
-
 $(function() {
 
     var pc = projectPlatform.getProjectCatalogue();
-
+    
     $("#showProjects")
             .button()
             .click(function() {
+        $("tbody").empty();
         var deferred = pc.get();
-
+        
         deferred.done(function(projects) {
-            $.each(projects.Project, function(index, p) {
-                console.log(p);
-                $("#data").append("<p>" + "date created: " + p.dateCreated +
-                        " id: " + p.id + " <br/>name: " + p.name +
-                        " admin: " + p.admin + "</p>");
+            console.log(projects);
+            $.each(projects.project, function(index, p) {
+                $("tbody").append("<tr> <td>" + p.name + "</td> <td>" + 
+                    p.dateCreated + "</td><td>" + p.admin.userName + "</td></tr>");
             });
             console.log(projects);
         });
@@ -21,52 +20,6 @@ $(function() {
             console.log("fail");
         });
     });
-    $("#addProject")
-            .button()
-            .click(function() {
-        createAddDialog();
-    });
-
-    function createAddDialog() {
-        clearValidationErrors();
-        clearFormDialogData();
-        var message = "Type in your projects name";
-        $("#dialog-form").dialog({
-            autoOpen: true,
-            modal: true,
-            stack: true,
-            buttons: {
-                "Save": function() {
-                    var project = getFormDialogData();
-
-                    pc.add(project.name);
-                    $(this).dialog("close");
-                },
-                "Cancel": function() {
-                    $(this).dialog("close");
-                }
-            }
-        });
-        $('#dialog-message').dialog('option', 'title', 'Delete project');
-        $("#dialog-message #msg").text(message);
-        return $('#dialog-message');
-    }
-
-    function clearValidationErrors() {
-        $("#dialog-form input").each(function() {
-            $(this).removeClass("ui-state-error");
-        });
-    }
-
-    function getFormDialogData() {
-        var project = {};
-        project.name = $("#dialog-form #name").val();
-        return project;
-    }
-
-    function clearFormDialogData() {
-        $("#dialog-form #name").val("");
-    }
 
 });
 
