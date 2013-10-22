8d90140ddbe66e022c5bc2373fbe9217bfa607c8
Eric Ahlberg <eahlberg@gmail.com>
2013-10-22 14:50:44 +0200

    Tests are back in the game

--- webbapp_model/src/test/java/com/adde/webbapp/model/ArticleTest.java
+++ webbapp_model/src/test/java/com/adde/webbapp/model/ArticleTest.java
@@ -1,126 +1,141 @@
-//package com.adde.webbapp.model;
-//
-//import com.adde.webbapp.model.dao.ArticleCatalogue;
-//import com.adde.webbapp.model.dao.DAOFactory;
-//import com.adde.webbapp.model.dao.PersonCatalogue;
-//import com.adde.webbapp.model.dao.ArticleEditCatalogue;
-//import com.adde.webbapp.model.entity.Article;
-//import com.adde.webbapp.model.entity.Person;
-//import com.adde.webbapp.model.entity.ArticleEdit;
-//import java.util.List;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//import org.junit.After;
-//import static org.junit.Assert.*;
-//import org.junit.Before;
-//import org.junit.Ignore;
-//import org.junit.Test;
-//
-//public class ArticleTest {
-//    DAOFactory daoFactory;
-//    PersonCatalogue personCatalogue;
-//    ArticleCatalogue articleCatalogue;
-//    ArticleEditCatalogue articleEditCatalogue;
-//    Article article1, article2;
-//    Person person1, person2;
-//    
-//    @Before
-//    public void before() {
-//        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- Before test!");
-//        daoFactory = DAOFactory.getDAOFactory();
-//        personCatalogue = daoFactory.getPersonCatalogue();
-//        articleCatalogue = daoFactory.getArticleCatalogue();
-//        articleEditCatalogue = daoFactory.getArticleEditCatalogue();
-//        person1 = new Person("person1", "email1", "pass1");
-//        personCatalogue.add(person1);
-//        person2 = new Person("person2", "email2", "pass1");
-//        personCatalogue.add(person2);
-//
-//        article1 = new Article("title1", "content1");
-//        article2 = new Article("title2", "content2");
-//        
-//        //Check
-//        assertTrue(personCatalogue.getAll().size() == 2);
-//        assertTrue(articleCatalogue.getAll().isEmpty());
-//    }
-//    
-//    @After
-//    public void after() {
-//        personCatalogue.remove(person1.getId());
-//        personCatalogue.remove(person2.getId());
-//        assertTrue(personCatalogue.getAll().isEmpty());
-//        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- After test!");
-//    }
-//    
-//    @Test
-//    public void addAndAuthorContent() {
-//        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- addAndAuthorContent!");
-//        articleCatalogue.add(article1);
-//        Article articleFromDB = articleCatalogue.find(article1.getId());
-//        Logger.getAnonymousLogger().log(Level.INFO, "Article1:{0}", articleFromDB.toString());
-//        
-//        //Correct Content?
-//        assertTrue(articleFromDB.getContent().equals("content1"));
-//        
-//        //Clean
-//        articleCatalogue.remove(articleFromDB.getId());
-//        assertTrue(articleCatalogue.getAll().isEmpty());
-//    }
-//    
-//    @Test
-//    public void testUpdatedEditorsAndFind() {
-//        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- testUpdatedEditorsAndFind!");
-//        articleCatalogue.add(article1);
-//        
-//        //New Editor
-//        Article articleFromDB = articleCatalogue.find(article1.getId());
-//        articleFromDB.setContent("updatedContent1");
-//        articleFromDB.setTitle("updatedTitle1");
-//        //Persist new ArticleEdit
-//        ArticleEdit see = new ArticleEdit(person2);
-//        articleEditCatalogue.add(see);
-//        //Add New ArticleEdit to Article
-//        List<ArticleEdit> editorEntry = articleFromDB.getArticleEditions();
-//        editorEntry.add(see);
-//        
-//        articleCatalogue.update(articleFromDB);
-//        
-//        // Check if the updated content is correct
-//        articleFromDB = articleCatalogue.find(article1.getId());
-//        assertTrue(articleFromDB.getTitle().equals("updatedTitle1"));
-//        assertTrue(articleFromDB.getContent().equals("updatedContent1"));
-//        
-//        // Check if the user has been added to the editors
-//        articleCatalogue.update(articleFromDB);
-//        articleFromDB = articleCatalogue.find(article1.getId());
-//        List<ArticleEdit> editorEntries = articleFromDB.getArticleEditions();
-//        assertTrue(editorEntries.size() == 1);
-//        assertTrue(editorEntries.get(0).getEditor().equals(person2));
-//        
-//        //getLatestEntry not working yet...
-//        /*Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- Before getLastEntry.................");
-//        List<SimpleEditorEntry> simpleEditorEntry = articleCatalogue.getLatestEntry(article1);
-//        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- After getLastEntry.................");
-//        assertTrue(simpleEditorEntry != null);
-//        assertTrue(simpleEditorEntry.get(0).getEditor().equals(person2));*/
-//
-//        //Clean
-//        articleCatalogue.remove(article1.getId());
-//        assertTrue(articleCatalogue.getAll().isEmpty());
-//        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- End testUpdatedEditorsAndFind!");
-//    }
-//    
-//    // Just for checking the printed output, not really a test
-//    @Test
-//    public void printArticle() {
-//        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- printArticle!");
-//        articleCatalogue.add(article1);
-//        Article articleFromDB = articleCatalogue.find(article1.getId());
-//        System.out.println(articleFromDB.toString());
-//        
-//        //Clean
-//        articleCatalogue.remove(article1.getId());
-//        assertTrue(articleCatalogue.getAll().isEmpty());
-//        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- End printArticle!");
-//    }
-//}
+package com.adde.webbapp.model;
+
+import com.adde.webbapp.model.dao.ArticleCatalogue;
+import com.adde.webbapp.model.dao.DAOFactory;
+import com.adde.webbapp.model.dao.PersonCatalogue;
+import com.adde.webbapp.model.dao.ArticleEditCatalogue;
+import com.adde.webbapp.model.dao.EMFUtil;
+import com.adde.webbapp.model.entity.Article;
+import com.adde.webbapp.model.entity.Person;
+import com.adde.webbapp.model.entity.ArticleEdit;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ArticleTest {
+    DAOFactory daoFactory;
+    PersonCatalogue personCatalogue;
+    ArticleCatalogue articleCatalogue;
+    ArticleEditCatalogue articleEditCatalogue;
+    Article article1, article2;
+    Person person1, person2;
+    
+    @BeforeClass
+    public static void createEMF() {
+        EMFUtil.createEntityManagerFactory();
+    }
+    
+    @AfterClass
+    public static void destroyEMF() {
+        EMFUtil.destroyEntityManagerFactory();
+    }
+    
+    @Before
+    public void before() {
+        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- Before test!");
+        daoFactory = DAOFactory.getDAOFactory();
+        
+        personCatalogue = daoFactory.getPersonCatalogue();
+        articleCatalogue = daoFactory.getArticleCatalogue();
+        articleEditCatalogue = daoFactory.getArticleEditCatalogue();
+        person1 = new Person("person1", "email1", "pass1");
+        personCatalogue.add(person1);
+        person2 = new Person("person2", "email2", "pass1");
+        personCatalogue.add(person2);
+
+        article1 = new Article("title1", "content1");
+        article2 = new Article("title2", "content2");
+        
+        //Check
+        assertTrue(personCatalogue.getAll().size() == 2);
+        assertTrue(articleCatalogue.getAll().isEmpty());
+    }
+    
+    @After
+    public void after() {
+        personCatalogue.remove(person1.getId());
+        personCatalogue.remove(person2.getId());
+        
+        assertTrue(personCatalogue.getAll().isEmpty());
+        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- After test!");
+    }
+    
+    @Test
+    public void addAndAuthorContent() {
+        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- addAndAuthorContent!");
+        articleCatalogue.add(article1);
+        Article articleFromDB = articleCatalogue.find(article1.getId());
+        Logger.getAnonymousLogger().log(Level.INFO, "Article1:{0}", articleFromDB.toString());
+        
+        //Correct Content?
+        assertTrue(articleFromDB.getContent().equals("content1"));
+        
+        //Clean
+        articleCatalogue.remove(articleFromDB.getId());
+        assertTrue(articleCatalogue.getAll().isEmpty());
+    }
+    
+    @Test
+    public void testUpdatedEditorsAndFind() {
+        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- testUpdatedEditorsAndFind!");
+        articleCatalogue.add(article1);
+        
+        //New Editor
+        Article articleFromDB = articleCatalogue.find(article1.getId());
+        articleFromDB.setContent("updatedContent1");
+        articleFromDB.setTitle("updatedTitle1");
+        //Persist new ArticleEdit
+        ArticleEdit see = new ArticleEdit(person2);
+        articleEditCatalogue.add(see);
+        //Add New ArticleEdit to Article
+        List<ArticleEdit> editorEntry = articleFromDB.getArticleEditions();
+        editorEntry.add(see);
+        
+        articleCatalogue.update(articleFromDB);
+        
+        // Check if the updated content is correct
+        articleFromDB = articleCatalogue.find(article1.getId());
+        assertTrue(articleFromDB.getTitle().equals("updatedTitle1"));
+        assertTrue(articleFromDB.getContent().equals("updatedContent1"));
+        
+        // Check if the user has been added to the editors
+        articleCatalogue.update(articleFromDB);
+        articleFromDB = articleCatalogue.find(article1.getId());
+        List<ArticleEdit> editorEntries = articleFromDB.getArticleEditions();
+        assertTrue(editorEntries.size() == 1);
+        assertTrue(editorEntries.get(0).getEditor().equals(person2));
+        
+        //getLatestEntry not working yet...
+        /*Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- Before getLastEntry.................");
+        List<SimpleEditorEntry> simpleEditorEntry = articleCatalogue.getLatestEntry(article1);
+        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- After getLastEntry.................");
+        assertTrue(simpleEditorEntry != null);
+        assertTrue(simpleEditorEntry.get(0).getEditor().equals(person2));*/
+
+        //Clean
+        articleCatalogue.remove(article1.getId());
+        assertTrue(articleCatalogue.getAll().isEmpty());
+        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- End testUpdatedEditorsAndFind!");
+    }
+    
+    // Just for checking the printed output, not really a test
+    @Test
+    public void printArticle() {
+        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- printArticle!");
+        articleCatalogue.add(article1);
+        Article articleFromDB = articleCatalogue.find(article1.getId());
+        System.out.println(articleFromDB.toString());
+        
+        //Clean
+        articleCatalogue.remove(article1.getId());
+        assertTrue(articleCatalogue.getAll().isEmpty());
+        Logger.getAnonymousLogger().log(Level.INFO, "----TEST---- End printArticle!");
+    }
+}
--- webbapp_model/src/test/java/com/adde/webbapp/model/PersonTest.java
+++ webbapp_model/src/test/java/com/adde/webbapp/model/PersonTest.java
@@ -1,98 +1,111 @@
-//package com.adde.webbapp.model;
-//
-//import com.adde.webbapp.model.dao.DAOFactory;
-//import com.adde.webbapp.model.dao.PersonCatalogue;
-//import com.adde.webbapp.model.entity.Person;
-//import java.util.List;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//import org.junit.After;
-//
-//import static org.junit.Assert.*;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-///**
-// * Test class for Person.
-// *
-// * @author hannes
-// */
-//public class PersonTest {
-//    Person person1, person2;
-//    String person1_pw, person2_pw;
-//
-//    PersonCatalogue personCatalogue;
-//
-//    @Before
-//    public void before() {
-//        DAOFactory daoFactory = DAOFactory.getDAOFactory();
-//        personCatalogue = daoFactory.getPersonCatalogue();
-//        
-//        person1_pw = "HsdE3324!gh";
-//        person1 = new Person("testperson_username", "testperson@testpersons.com", person1_pw);
-//        person1.setFirstName("person1_firstname");
-//        person1.setLastName("person1_lastname");
-//        
-//        person2_pw = "YhIJKd!ad";
-//        person2 = new Person("testperson 2", "testperson2@testpersons.com", person2_pw);
-//        
-//        personCatalogue.add(person1);
-//        personCatalogue.add(person2);
-//        
-//        List<Person> allPersons = personCatalogue.getAll();
-//        
-//        assertTrue(allPersons.contains(person1) && allPersons.contains(person2) && allPersons.size() == 2);
-//    }
-//    
-//    @After
-//    public void after() {
-//        List<Person> allPersons = personCatalogue.getAll();
-//        for(Person p : allPersons) {
-//            personCatalogue.remove(p.getId());
-//        }
-//        
-//        assertTrue(personCatalogue.getCount() == 0);
-//    }
-//
-//    @Test
-//    public void update() {
-//        String person2_updated_pw = "nEw!paSSwoRD";
-//        person2.setPassword(person2_updated_pw);
-//        personCatalogue.update(person2);
-//        assertTrue(personCatalogue.find(person2.getId()).getPassword().equals(person2_updated_pw));
-//    }
-//    
-//    @Test
-//    public void getBy() {
-//        assertTrue(personCatalogue.getByUserName(person1.getUserName()).equals(person1));
-//        assertTrue(personCatalogue.getByFirstName(person1.getFirstName()).contains(person1));
-//        assertTrue(personCatalogue.getByLastName(person1.getLastName()).contains(person1));
-//        assertTrue(personCatalogue.getByEmail(person1.getEmail()).equals(person1));
-//    }
-//    
-//    @Test
-//    public void equalityTest() {
-//        assertFalse(person1.equals(person2));
-//        assertFalse(person2.equals(person1));
-//
-//        assertTrue(person1.equals(person1));
-//    }
-//    
-//    @Test
-//    public void printUser() {
-//        Logger.getAnonymousLogger().log(Level.INFO, "Two users created with "
-//                + "attributes: {0} and {1}", new Object[]{person1, person2});
-//    }
-//    
-//    @Test
-//    public void getCount() {
-//        Logger.getAnonymousLogger().log(Level.INFO, "getCount: {0}", personCatalogue.getCount());
-//        assertTrue(personCatalogue.getCount() == 2);
-//        List<Person> personList = personCatalogue.getAll();
-//        for(Person p : personList){
-//            personCatalogue.remove(p.getId());
-//        }
-//        Logger.getAnonymousLogger().log(Level.INFO, "getCount: {0}", personCatalogue.getCount());
-//        assertTrue(personCatalogue.getCount() == 0);
-//    }
-//}
+package com.adde.webbapp.model;
+
+import com.adde.webbapp.model.dao.DAOFactory;
+import com.adde.webbapp.model.dao.EMFUtil;
+import com.adde.webbapp.model.dao.PersonCatalogue;
+import com.adde.webbapp.model.entity.Person;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Test class for Person.
+ *
+ * @author Eric Ahlberg (eahlberg@gmail.com)
+ */
+public class PersonTest {
+    Person person1, person2;
+    String person1_pw, person2_pw;
+
+    PersonCatalogue personCatalogue;
+
+    @BeforeClass
+    public static void createEMF() {
+        EMFUtil.createEntityManagerFactory();
+    }
+    
+    @AfterClass
+    public static void destroyEMF() {
+        EMFUtil.destroyEntityManagerFactory();
+    }
+    
+    @Before
+    public void before() {
+        DAOFactory daoFactory = DAOFactory.getDAOFactory();
+        personCatalogue = daoFactory.getPersonCatalogue();
+        
+        person1_pw = "HsdE3324!gh";
+        person1 = new Person("testperson_username", "testperson@testpersons.com", person1_pw);
+        person1.setFirstName("person1_firstname");
+        person1.setLastName("person1_lastname");
+        
+        person2_pw = "YhIJKd!ad";
+        person2 = new Person("testperson 2", "testperson2@testpersons.com", person2_pw);
+        
+        personCatalogue.add(person1);
+        personCatalogue.add(person2);
+        
+        List<Person> allPersons = personCatalogue.getAll();
+        
+        assertTrue(allPersons.contains(person1) && allPersons.contains(person2) && allPersons.size() == 2);
+    }
+    
+    @After
+    public void after() {
+        List<Person> allPersons = personCatalogue.getAll();
+        for(Person p : allPersons) {
+            personCatalogue.remove(p.getId());
+        }
+        
+        assertTrue(personCatalogue.getCount() == 0);
+    }
+
+    @Test
+    public void update() {
+        String person2_updated_pw = "nEw!paSSwoRD";
+        person2.setPassword(person2_updated_pw);
+        personCatalogue.update(person2);
+        assertTrue(personCatalogue.find(person2.getId()).getPassword().equals(person2_updated_pw));
+    }
+    
+    @Test
+    public void getBy() {
+        assertTrue(personCatalogue.getByUserName(person1.getUserName()).equals(person1));
+        assertTrue(personCatalogue.getByFirstName(person1.getFirstName()).contains(person1));
+        assertTrue(personCatalogue.getByLastName(person1.getLastName()).contains(person1));
+        assertTrue(personCatalogue.getByEmail(person1.getEmail()).equals(person1));
+    }
+    
+    @Test
+    public void equalityTest() {
+        assertFalse(person1.equals(person2));
+        assertFalse(person2.equals(person1));
+
+        assertTrue(person1.equals(person1));
+    }
+    
+    @Test
+    public void printUser() {
+        Logger.getAnonymousLogger().log(Level.INFO, "Two users created with "
+                + "attributes: {0} and {1}", new Object[]{person1, person2});
+    }
+    
+    @Test
+    public void getCount() {
+        Logger.getAnonymousLogger().log(Level.INFO, "getCount: {0}", personCatalogue.getCount());
+        assertTrue(personCatalogue.getCount() == 2);
+        List<Person> personList = personCatalogue.getAll();
+        for(Person p : personList){
+            personCatalogue.remove(p.getId());
+        }
+        Logger.getAnonymousLogger().log(Level.INFO, "getCount: {0}", personCatalogue.getCount());
+        assertTrue(personCatalogue.getCount() == 0);
+    }
+}
